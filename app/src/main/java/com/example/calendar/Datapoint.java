885b22b08e02ad6495a3063450bdade2564ac75a
mariiaar <maria.aartolahti@student.lut.fi>
2022-04-18 15:50:19 +0300

    Edited: Tree editTexts, Button saves to calendar view and better layout

---
+++ app/src/main/java/com/example/calendar/Datapoint.java
@@ -0,0 +1,13 @@
+package com.example.calendar;
+
+public class Datapoint {
+    public String event;
+    public String place;
+    public String review;
+
+    public Datapoint(String e, String p, String r) {
+        this.event = e;
+        this.place = p;
+        this.review = r;
+    }
+}
--- app/src/main/java/com/example/calendar/MainActivity.java
+++ app/src/main/java/com/example/calendar/MainActivity.java
@@ -1,23 +1,31 @@
 package com.example.calendar;
 
+import android.os.Build;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.EditText;
 import android.os.Bundle;
+import java.time.LocalDate;
+import java.util.HashMap;
+
 import android.text.InputType;
 import android.view.View;
 import android.os.Bundle;
 
 
-
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button;
+    Button buttonSave;
     EditText editText;
+    EditText editText2;
+    EditText editText3;
     CalendarView calendarView;
-    String selectedDate;
+    LocalDate selectedDate;
+
+    HashMap<LocalDate, Datapoint> events;
 
 
     @Override
@@ -25,14 +33,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         editText = findViewById(R.id.editText);
+        editText2 = findViewById(R.id.editText2);
+        editText3 = findViewById(R.id.editText3);
         calendarView = findViewById(R.id.calendarView);
-        //editText.setInputType(InputType.TYPE_NULL);
-        //editText.setOnClickListener(new View.OnClickListener() {
+        buttonSave=findViewById(R.id.buttonSave);
+        events = new HashMap<LocalDate, Datapoint>();
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
-                selectedDate = Integer.toString(year) + Integer.toString(month) + Integer.toString(dayOfMonth);
+                selectedDate = LocalDate.of(year, month, dayOfMonth);
+                Datapoint dateData = events.get(selectedDate);
+                if (dateData != null) {
+                    editText.setText(dateData.event);
+                    editText2.setText(dateData.place);
+                    editText3.setText(dateData.review);
+                } else {
+                    editText.setText("");
+                    editText2.setText("");
+                    editText3.setText("");
+                }
             }
-            //@Override
-            //public void onClick(View v) {
+        });
+
+        buttonSave.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Datapoint data = new Datapoint(
+                        editText.getText().toString(),
+                        editText2.getText().toString(),
+                        editText3.getText().toString()
+
+                );
+                events.put(selectedDate, data);
+            }
+        });
+    }
 }
