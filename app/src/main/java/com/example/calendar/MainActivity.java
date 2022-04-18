70c841f7cca4ed2bdf42cddddc4512c12efdb1ca
mariiaar <maria.aartolahti@student.lut.fi>
2022-04-18 17:01:44 +0300

    Save and loaded values from file.

--- app/src/main/java/com/example/calendar/MainActivity.java
+++ app/src/main/java/com/example/calendar/MainActivity.java
@@ -5,18 +5,26 @@ import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.EditText;
 import android.os.Bundle;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
 import java.time.LocalDate;
 import java.util.HashMap;
+import java.util.Scanner;
 
 import android.text.InputType;
 import android.view.View;
 import android.os.Bundle;
+import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
 
-
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private static String FILE_NAME = "events.json";
 
     Button buttonSave;
     EditText editText;
@@ -25,8 +33,7 @@ public class MainActivity extends AppCompatActivity {
     CalendarView calendarView;
     LocalDate selectedDate;
 
-    HashMap<LocalDate, Datapoint> events;
-
+    HashMap<String, Datapoint> events;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,12 +44,15 @@ public class MainActivity extends AppCompatActivity {
         editText3 = findViewById(R.id.editText3);
         calendarView = findViewById(R.id.calendarView);
         buttonSave=findViewById(R.id.buttonSave);
-        events = new HashMap<LocalDate, Datapoint>();
+        events = new HashMap<String, Datapoint>();
+
+        selectedDate = LocalDate.now();
+
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
                 selectedDate = LocalDate.of(year, month, dayOfMonth);
-                Datapoint dateData = events.get(selectedDate);
+                Datapoint dateData = events.get(selectedDate.toString());
                 if (dateData != null) {
                     editText.setText(dateData.event);
                     editText2.setText(dateData.place);
@@ -63,8 +73,47 @@ public class MainActivity extends AppCompatActivity {
                         editText3.getText().toString()
 
                 );
-                events.put(selectedDate, data);
+                events.put(selectedDate.toString(), data);
+                Gson gson = new Gson();
+                String json = gson.toJson(events);
+                System.out.println(json);
+                try {
+                    saveFile(json);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
+
+        try {
+            events = readFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private void saveFile(String json) throws IOException {
+        String filePath = new File(this.getFilesDir(), FILE_NAME).getPath();
+        FileWriter fw = new FileWriter(filePath);
+
+        fw.write(json);
+        fw.close();
+    }
+
+    private HashMap<String, Datapoint> readFile() throws IOException {
+        File file = new File(this.getFilesDir(), FILE_NAME);
+        Scanner sc = new Scanner(file);
+
+        StringBuilder sb = new StringBuilder();
+
+        while (sc.hasNextLine()) {
+            sb.append(sc.nextLine());
+        }
+
+        String json = sb.toString();
+
+        // From Gson documentation: https://github.com/google/gson/blob/master/UserGuide.md
+        Gson gson = new Gson();
+        Type targetType = new TypeToken<HashMap<String, Datapoint>>(){}.getType();
+        return gson.fromJson(json, targetType);
     }
 }
