8a830aaa1fad02443469733d11d0598e45e359a7
mariiaar <maria.aartolahti@student.lut.fi>
2022-04-26 13:23:44 +0300

    Save and loaded values from file. + comments

--- app/src/main/java/com/example/calendar/MainActivity.java
+++ app/src/main/java/com/example/calendar/MainActivity.java
@@ -34,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
     LocalDate selectedDate;
 
     HashMap<String, Datapoint> events;
+    //https://www.w3schools.com/java/java_hashmap.asp
+    //Using HashMap for storing items in value pairs, and accessing them by an index of another type (=String).
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,8 +47,12 @@ public class MainActivity extends AppCompatActivity {
         calendarView = findViewById(R.id.calendarView);
         buttonSave=findViewById(R.id.buttonSave);
         events = new HashMap<String, Datapoint>();
+        //Create new HasMap called "events"
+        //findViewById is the method that finds the View by the ID which it has given.
 
         selectedDate = LocalDate.now();
+        //Java LocalDate represents Date with a default format of yyyy-mm-dd.
+        //LocalDate.now() method of a LocalDate class used to obtain the current date.
 
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
@@ -64,7 +70,9 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-
+        //If there are something written in CalendarView's EditTexts
+        // -> setText() is used to set the new text into calendar
+        // If there isn't writen anything -> it'll be empty
         buttonSave.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Datapoint data = new Datapoint(
@@ -91,6 +99,14 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
     }
+    //The data (the text) will be stored when user clicks "Tallenna"-button
+    //getText() Returns the text of the text field.
+    //And because it must be on string -> toString() method represent any object as a string
+    //Have to use JSON format for serializing and transmitting structured data (in this case "events")
+    //Gson is a Java library that can be used to modulate Java objects into their JSON representation.
+    //Without using JSON and GSON it isn't possible to save in correct form.
+    //Saveing the data into file or reading file need the try-catch method of handling exceptions.
+
     private void saveFile(String json) throws IOException {
         String filePath = new File(this.getFilesDir(), FILE_NAME).getPath();
         FileWriter fw = new FileWriter(filePath);
@@ -98,7 +114,7 @@ public class MainActivity extends AppCompatActivity {
         fw.write(json);
         fw.close();
     }
-
+    //Writing into file needs use the FileWriter. The file is used for storing data.
     private HashMap<String, Datapoint> readFile() throws IOException {
         File file = new File(this.getFilesDir(), FILE_NAME);
         Scanner sc = new Scanner(file);
@@ -108,7 +124,7 @@ public class MainActivity extends AppCompatActivity {
         while (sc.hasNextLine()) {
             sb.append(sc.nextLine());
         }
-
+        //Line by line
         String json = sb.toString();
 
         // From Gson documentation: https://github.com/google/gson/blob/master/UserGuide.md
@@ -117,3 +133,6 @@ public class MainActivity extends AppCompatActivity {
         return gson.fromJson(json, targetType);
     }
 }
+//StringBuilder is used to modify Java strings in-place.
+//A file path defines the location of a file inside a folder structure -> finds the right file.
+// TypeToken -> transfer a HashMap via Gson
